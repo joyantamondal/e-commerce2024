ab14c61cbd89e4aa2accf592910bd271c194014f
joyantamondal <engr.joyantamondal@gmail.com>
2024-04-09 00:20:40 +0600

    loginAdded

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,10 +1,21 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({
     user: null,
     token: "",
   });
+  useEffect(() => {
+    const data = localStorage.getItem("auth");
+    if (data) {
+      const parseData = JSON.parse(data);
+      setAuth({
+        ...auth,
+        user: parseData.user,
+        token: parseData.token,
+      });
+    }
+  }, [auth]);
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
       {children}
--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -3,10 +3,12 @@ import Layout from "./../../components/Layout/Layout";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { useAuth } from "../../context/auth";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
   const navigate = useNavigate();
 
   // form function
@@ -19,6 +21,12 @@ export const Login = () => {
       );
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
         navigate("/");
       } else {
         toast.error(res.data.message);
