11a3d5f1599e85c3cf344e40b13f529fc700fab4
joyantamondal <engr.joyantamondal@gmail.com>
2024-04-25 00:44:32 +0600

    backendProductCatAdded

---
+++ routes/productRoutes.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/productController.js";
+import formidable from "express-formidable";
+const router = express.Router();
+//routes
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+//  get All Product
+router.get("/get-product", getProductController);
+
+//  get single Product
+router.get("/get-product/:slug", getSingleProductController);
+
+//  get  Photo
+router.get("/product-photo/:pid", productPhotoController);
+
+// delete Product
+router.delete("/product/:pid", deleteProductController);
+
+//update Product
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+export default router;
